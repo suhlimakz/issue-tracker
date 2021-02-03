a891d90f49d58d45db192a311f583b32e854c1c2
suhlimakz <suh.lima.kz@gmail.com>
2021-02-03 16:47:19 -0300

    API creating

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,1 +1,14 @@
+const app = require( 'express' )();
+const port = 3001;
 
+app.get( '/', function( req, res  ) {
+  res.json( { ping: 'pong' } );
+} );
+
+app.get( '/user/login', function( req, res ) {
+  res.send(  )
+} )
+
+app.listen( port, function() {
+  console.log( `I live in port  ${port}` );
+} );
