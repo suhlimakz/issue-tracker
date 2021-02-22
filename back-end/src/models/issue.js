38b0c9ba4e034bf8fa462b13c27273634397d0e6
suhlimakz <suh.lima.kz@gmail.com>
2021-02-22 14:30:55 -0300

    Creating query in model

--- back-end/src/models/issue.js
+++ back-end/src/models/issue.js
@@ -1,7 +1,26 @@
 const { getConnection } = require( '../models/connector/mysql' );
 
-async function addIssue() {
+async function addIssue( issue ) {
+  const { title, createBy, dueDate, priority, status, assignee, releaseId, description } = issue;
 
+  const connection = await getConnection();
+  const sql = `INSERT INTO issue( title, createBy, dueDate, priority, status, assignee, releaseId, description )
+                      VALUES (
+                        '${ title }',
+                        '${ createBy }',
+                        '${ dueDate }',
+                        '${ priority }',
+                        '${ status }',
+                        '${ assignee }',
+                        '${ releaseId }',
+                        '${ description }'
+                      );`
+
+  const [ results ] = await connection.query( sql );
+
+  connection.end();
+
+  return results;
 }
 
 module.exports = {
