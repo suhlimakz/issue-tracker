c8dba96308c2daf0eb53c50e111d229af026def5
suhlimakz <suh.lima.kz@gmail.com>
2021-02-22 14:59:22 -0300

    Creating route in controller

--- back-end/src/controllers/issue.js
+++ back-end/src/controllers/issue.js
@@ -1,7 +1,19 @@
 const issueService = require( '../services/issue' );
 
 async function newIssue( req, res, next ) {
-  
+  const { title, createBy, dueDate, priority, status, assignee, releaseId, description } = req.body;
+  const issue = {
+    title,
+    createBy, 
+    dueDate,
+    priority,
+    status,
+    assignee,
+    releaseId,
+    description
+  }
+
+  return res.json( await issueService.addIssue( issue ) );
 }
 
 function set( app ) {
--- back-end/src/services/issue.js
+++ back-end/src/services/issue.js
@@ -15,7 +15,6 @@ async function addIssue( issue ) {
     return msg;
   }
 
-  console.log( msg );
   return { ...msg, ...registerIssue };
 }
 
