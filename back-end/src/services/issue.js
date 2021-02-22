4abbcd0504ca3c3dad5c9a49aef1f58a88f03599
suhlimakz <suh.lima.kz@gmail.com>
2021-02-22 14:35:48 -0300

    Creating rule in service

--- back-end/src/services/issue.js
+++ back-end/src/services/issue.js
@@ -1,7 +1,22 @@
-const userModel = require('../models/issue');
+const issueModel = require('../models/issue');
 
-async function addIssue() {
+async function addIssue( issue ) {
+  const msg = {
+    register: true,
+    info: '' 
+  }
 
+  let registerIssue = await issueModel.addIssue( issue );
+
+  if( !registerIssue ) {
+    msg.register = false;
+    msg.info = 'failed register user';
+
+    return msg;
+  }
+
+  console.log( msg );
+  return { ...msg, ...registerIssue };
 }
 
 module.exports = {
