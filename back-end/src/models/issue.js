33beac83d08669b34b4166f65e57e94ccf2b8b66
suhlimakz <suh.lima.kz@gmail.com>
2021-03-02 17:54:36 -0300

    Add sql for update issue in model

--- back-end/src/models/issue.js
+++ back-end/src/models/issue.js
@@ -34,7 +34,33 @@ async function deleteIssue( id ) {
   return results;
 }
 
+async function updateIssue( issue ) {
+  const { id, title, createBy, dueDate, priority, status, assignee, releaseId, description } = issue;
+  
+  const connection = await getConnection();
+
+  const sql = `UPDATE issue
+               SET
+                title = '${ title }',
+                createBy = ${ createBy },
+                dueDate = '${ dueDate }',
+                priority = ${ priority },
+                status = ${ status },
+                assignee = ${ assignee },
+                releaseId = ${ releaseId },
+                description = '${ description }'
+               WHERE id = ${ id };`;
+
+  const [ results ] = await connection.query( sql );
+
+  connection.end();
+  
+  console.log( 'update successful' );
+  return results;
+}
+
 module.exports = {
   addIssue,
   deleteIssue,
+  updateIssue,
 };
