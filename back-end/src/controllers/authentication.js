5df28bc607997e78eddcf18f77acf79c8923cd04
suhlimakz <suh.lima.kz@gmail.com>
2021-04-15 15:31:51 -0300

    Token validating

---
+++ back-end/src/controllers/authentication.js
@@ -0,0 +1,32 @@
+const jwt = require( "jsonwebtoken" );
+const privateKey = 'secretkey';
+
+function getToken( username, password ){
+  const token = jwt.sign( { username, password }, privateKey, {
+    expiresIn: "1m",
+  } );
+
+  return token;
+}
+
+function validateToken( req, res, next ) {
+  const token = req.header( 'token' );
+
+  if( req.originalUrl == '/login' ) {
+    return next();
+  }
+
+  if( !token ) {
+    return res.status( 401 ).json( { error: 'Not logged in' } );
+  }
+
+  try{
+    req.decoded = jwt.verify( token, privateKey );
+
+    return next();
+  } catch( e ) {
+    console.log( e );
+
+    return res.status( 403 ).json( { error: 'Unauthorized' } );
+  }
+}
