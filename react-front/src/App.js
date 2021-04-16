09c5e57f7e413b8a4b66a95283b260e3dd015416
suhlimakz <suh.lima.kz@gmail.com>
2021-04-16 17:03:40 -0300

    Insert icons

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +18,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+
     </div>
   );
 }
--- react-front/src/components/header/index.js
+++ react-front/src/components/header/index.js
@@ -1,5 +1,7 @@
 import './index.css';
-import { SearchIcon } from '@material-ui/icons';
+import SearchIcon  from '@material-ui/icons/Search';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
   return(
@@ -10,25 +12,27 @@ function Header() {
 
       <div className="search--area">
         <div className="search--icon">
-
+          <SearchIcon></SearchIcon>
         </div>
         <input type="text" placeholder="Search..."/>
       </div>
 
       <div className="notification--area">
-
+        <NotificationsIcon></NotificationsIcon>
       </div>
 
       <div className="user--area">
         <div className="icon--area">
-
+          <AccountCircleIcon></AccountCircleIcon>
         </div>
         <div className="identify--user">
           <h1 className="user--name">Username</h1>
           <p className="user--occupation">Occupation</p>
         </div>
       </div>
-      
+
     </header>
   )
 }
+
+export default Header;
