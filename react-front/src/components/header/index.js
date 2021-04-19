e2a8cdbde42ee3fcd89a976c5510932a45724866
suhlimakz <suh.lima.kz@gmail.com>
2021-04-19 16:02:03 -0300

    Align header

--- react-front/src/components/header/index.js
+++ react-front/src/components/header/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import SearchIcon  from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -5,33 +7,33 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
   return(
-    <header className="container">
-      <div className="logo-area">
-        <div className="logo-image"></div>
-      </div>
-
-      <div className="search--area">
-        <div className="search--icon">
-          <SearchIcon></SearchIcon>
+    <header className="Header-container">
+        <div className="logo--area">
+          <div className="logo--image"></div>
         </div>
-        <input type="text" placeholder="Search..."/>
-      </div>
 
-      <div className="notification--area">
-        <NotificationsIcon></NotificationsIcon>
-      </div>
-
-      <div className="user--area">
-        <div className="icon--area">
-          <AccountCircleIcon></AccountCircleIcon>
+        <div className="search--area">
+          <div className="search--icon">
+            <SearchIcon></SearchIcon>
+          </div>
+          <input type="text" placeholder="Search..."/>
         </div>
-        <div className="identify--user">
-          <h1 className="user--name">Username</h1>
-          <p className="user--occupation">Occupation</p>
-        </div>
-      </div>
 
-    </header>
+        <div className="notification--area">
+          <NotificationsIcon></NotificationsIcon>
+        </div>
+
+        <div className="user--area">
+          <div className="icon--area">
+            <AccountCircleIcon></AccountCircleIcon>
+          </div>
+          <div className="identify--user">
+            <h1 className="user--name">Username</h1>
+            <p className="user--occupation">Occupation</p>
+          </div>
+        </div>
+
+      </header>
   )
 }
 
