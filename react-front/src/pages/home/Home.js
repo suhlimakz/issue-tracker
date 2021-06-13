ab929324b292c68481d0dbfbe3e1f5a7d7b054e1
suhlimakz <suh.lima.kz@gmail.com>
2021-06-13 17:15:00 -0300

    Home page created

--- react-front/src/pages/home/Home.js
+++ react-front/src/pages/home/Home.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Header from '../../components/header';
+import Menu from '../../components/menu';
 
+function Home() {
+  return(
+    <div>
+      <Header />
+      <Menu />   
+    </div>
+  )
+}
+
+export default Home;
