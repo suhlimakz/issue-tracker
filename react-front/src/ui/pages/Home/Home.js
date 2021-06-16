a3630402674d898f08f4ee2efce5f6b49b87f191
suhlimakz <suh.lima.kz@gmail.com>
2021-06-16 19:57:24 -0300

    Files moved for others folders

---
+++ react-front/src/ui/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/surfaces/Header/Header';
+import Menu from '../../components/surfaces/Menu/Menu';
+
+function Home() {
+  return(
+    <div>
+      <Header />
+      <Menu />   
+    </div>
+  )
+}
+
+export default Home;
