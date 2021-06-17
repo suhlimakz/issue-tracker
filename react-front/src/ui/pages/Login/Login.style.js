c388d48c16fc089dd575a36ae11bae929070aa08
suhlimakz <suh.lima.kz@gmail.com>
2021-06-17 09:41:30 -0300

    Login page styles file | ConteinerLogin created

---
+++ react-front/src/ui/pages/Login/Login.style.js
@@ -0,0 +1,10 @@
+import { styled } from "@material-ui/styles";
+import { Container } from "@material-ui/core";
+
+export const LoginContainer = styled( Container )({
+  backgroundColor: '#cfe8fc',
+  display: 'flex',
+  justifyContent: 'center',
+  maxWidth: 'md'
+})
+
